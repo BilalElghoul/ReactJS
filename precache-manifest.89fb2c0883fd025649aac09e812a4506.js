ed91b5d041a08b9cf72997c5fb4ad6e0b93bd3df
BilalElGHOUL-Tech <bilaltec72@gmail.com>
2020-03-27 21:13:50 +0100

    Updates

---
+++ precache-manifest.89fb2c0883fd025649aac09e812a4506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a9312c0a92e76716236710f5ac58ee",
+    "url": "/BilalElghoul/ReactJS/index.html"
+  },
+  {
+    "revision": "88d5c35451042a5449ee",
+    "url": "/BilalElghoul/ReactJS/static/css/main.7ddc5b72.chunk.css"
+  },
+  {
+    "revision": "ef184dcc66e80e73a87c",
+    "url": "/BilalElghoul/ReactJS/static/js/2.a29589f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BilalElghoul/ReactJS/static/js/2.a29589f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d5c35451042a5449ee",
+    "url": "/BilalElghoul/ReactJS/static/js/main.79001d98.chunk.js"
+  },
+  {
+    "revision": "557e5f54536dc8890877",
+    "url": "/BilalElghoul/ReactJS/static/js/runtime-main.b36097b2.js"
+  }
+]);
